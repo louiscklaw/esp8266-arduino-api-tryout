9dab5303a408fb6be04a4eb2e09d1c071478f776
logickee <logickee@gmail.com>
2019-05-08 16:34:50 +0800

    update,

---
+++ esp8266-arduino-api-tryout.ino
@@ -0,0 +1,15 @@
+
+
+ADC_MODE(ADC_VCC);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("current voltage: ");
+  Serial.println(ESP.getVcc());
+  delay(1000);
+}
